23a9fb8ce88b02a9fef530d710e78275a457692f
bachelorwhc <bachelor.whc@gmail.com>
2014-10-17 17:18:35 +0800

    Kadane algorithms added.

--- algorithms/Kadane's algorithm/kadane.cpp
+++ algorithms/Kadane's algorithm/kadane.cpp
@@ -1,36 +1,4 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#define MATRIXINFO matrix, size
-
-struct Group
-{
-	Group(short sx, short ex, short sy, short ey) :
-		x1(sx), x2(ex), y1(sy), y2(ey)
-	{
-
-	};
-	short x1;
-	short x2;
-	short y1;
-	short y2;
-};
-
-short GetElement(short int* matrix, unsigned short size, unsigned short x, unsigned short y)
-{
-	return matrix[x + y * size];
-}
-
-long long GroupCount(short int* matrix, short size, Group& g)
-{
-	if (g.x1 > g.x2 || g.y1 > g.y2 || g.x1 >= size || g.x1 >= size || g.y1 < 0 || g.y2 < 0 || g.x1 < 0 || g.y1 < 0 || g.x2 >= size || g.y2 >= size)
-		return -127000000;
-	long long sum(0);
-	for (short x = g.x1; x <= g.x2; ++x)
-		for (short y = g.y1; y <= g.y2; ++y)
-			sum += GetElement(MATRIXINFO, x, y);
-	return sum;
-}
 
 long FindMaxSubarray(short int* matrix, unsigned short size, unsigned short& left_of_max, unsigned short& right_of_max)
 {
@@ -65,13 +33,10 @@ int main()
 	unsigned short size;
 	std::cin >> size;
 	short int* matrix = new short int[size];
-	//for (unsigned short y = 0; y < size; ++y)
 	for (unsigned short x = 0; x < size; ++x)
-		//std::cin >> matrix[x + y * size];
 			std::cin >> matrix[x];
 	unsigned short left_of_max(0);
 	unsigned short right_of_max(0);
 	std::cout << FindMaxSubarray(matrix, size, left_of_max, right_of_max) << std::endl;
-	//std::cout << ' ' << left_of_max << ' ' << right_of_max << std::endl;
 	return 0;
 }
