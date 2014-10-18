66dbb64838aea4b1e76101c041f9b0c28211099e
bachelorwhc <bachelor.whc@gmail.com>
2014-10-18 10:19:59 +0800

    10110 still unsolved.

--- 10110/10110.cpp
+++ 10110/10110.cpp
@@ -4,10 +4,24 @@
 
 int main()
 {
-	float lights;
+	double lights;
 	while (std::cin >> lights)
 	{
 		bool isl(false);
+		unsigned int limit(ceil(sqrt(lights)));
+
+		if(((unsigned int)floor(lights)) == 0)
+			break;
+		if((unsigned int)lights == 1)
+			isl = true;
+		if((unsigned int)lights == 4)
+			isl = true;
+
+		for(unsigned int laps = 3; laps <= limit; ++laps)
+		{
+			if((unsigned int)lights % laps == 0)
+				isl = !isl;
+		}
 		if(isl)
 			std::cout << "yes" << std::endl;
 		else
