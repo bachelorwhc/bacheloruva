8709a4149ebe0158d17e4cb8856c370d7ad49154
bachelor.whc <bachelor.whc@gmail.com>
2014-10-19 13:48:42 +0800

    10110 solved.

--- 10110/10110.cpp
+++ 10110/10110.cpp
@@ -1,17 +1,13 @@
 #include <iostream>
-#include <algorithm>
 #include <cmath>
 
 int main()
 {
-	float lights;
-	while (std::cin >> lights)
+	unsigned int lights;
+	while (std::cin >> lights, lights)
 	{
-		bool isl(false);
-		if(isl)
-			std::cout << "yes" << std::endl;
-		else
-			std::cout << "no" << std::endl;
+		int t(sqrt(lights));
+		std::cout << ((t * t == lights) ? "yes" : "no") << std::endl;
 	}
 	return 0;
 }
