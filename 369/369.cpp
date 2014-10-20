90b1f63cc178723db3c3aa68d6421253c3d72858
bachelor.whc <bachelor.whc@gmail.com>
2014-10-20 23:49:30 +0800

    369 unsolved.

--- 369/369.cpp
+++ 369/369.cpp
@@ -6,33 +6,23 @@ int main()
 	unsigned long long m, n;
 	while (std::cin >> n >> m)
 	{
+		unsigned long long bm(m);
 		if(n == 0 && m == 0)
 			break;
+		if(n == m)
+		{
+			std::cout << n << " things taken " << m << " at a time is " << 1 << " exactly." << std::endl;
+			continue;
+		}
 		if(n - m < m)
 			m = n - m;
 		unsigned long long a(n - m + 1), b(1);
-		unsigned int doublecount(0);
 		for(unsigned long long i = 2; (n - m) + i <= n; ++i)
-		{
-			if((n - m + i) % 2 == 0)
-			{
-				a *= ((n - m + i) / 2);
-				++doublecount;
-			}
-			else
 				a *= (n - m + i);
-		}
 		for(unsigned long long i = 2; i <= m; ++i)
-		{
-			if(i % 2 == 0 && doublecount >= 0)
-			{
-				b *= (i / 2);
-				--doublecount;
-			}
-			else
 				b *= i;
-		}
-		std::cout << n << " things taken " << m << " at a time is " << a / b << " exactly." << std::endl;
+		unsigned long long ans(a/b);
+		std::cout << n << " things taken " << bm << " at a time is " << ans << " exactly." << std::endl;
 	}
 	return 0;
 }
