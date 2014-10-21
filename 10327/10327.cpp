118b8f55cfd138591ddc64745da2d10443d8ddd2
bachelorwhc <bachelor.whc@gmail.com>
2014-10-21 12:40:07 +0800

    10327 solved.

--- 10327/10327.cpp
+++ 10327/10327.cpp
@@ -2,17 +2,38 @@
 #include <algorithm>
 #include <vector>
 
+bool IsOrder(std::vector<int>& ns)
+{
+	unsigned int len(ns.size());
+	for(unsigned int i = 0; i + 1< len; ++i)
+	{
+		if(ns[i] > ns[i + 1])
+			return false;
+	}
+	return true;
+}
+
 int main()
 {
 	unsigned short l;
 	while (std::cin >> l)
 	{
-		std::vector<int> t(l);
+		std::vector<int> ns(l);
 		for(int j = 0; j < l; ++j)
+			std::cin >> ns[j];
+		unsigned int count(0);
+		while (!IsOrder(ns))
 		{
-			std::cin >> t[j];
+			for(unsigned int i = 0; i + 1 < l; ++i)
+			{
+				if(ns[i] > ns[i + 1])
+				{
+					std::swap(ns[i], ns[i + 1]);
+					++count;
+				}
+			}
 		}
-		std::cout << "Minimum exchange operations : " << ' ' << std::endl;
+		std::cout << "Minimum exchange operations : " << count << std::endl;
 	}
 	return 0;
 }
