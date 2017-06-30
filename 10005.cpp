24fe54e1e6d1a0483de372ae723ccc6b0496e45c
bachelor.whc@gmail.com <bachelor.whc@gmail.com>
2017-06-30 14:59:52 +0800

    serious bug fixed.

--- 10005.cpp
+++ 10005.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <vector>
-#include <cfloat>
 #include <cmath>
-#include <climits>
-#include <algorithm>
+#include <cfloat>
+#include <limits>
+
+const double epsilon = std::numeric_limits<double>::epsilon();
 
 struct Vec {
 	Vec() : x(0), y(0) {
@@ -22,8 +23,16 @@ struct Vec {
 		return Vec(x + v.x, y + v.y);
 	}
 
+	Vec operator-(const Vec& v) const {
+		return Vec(x - v.x, y - v.y);
+	}
+
 	Vec operator/(const double s) const {
 		return Vec(x/s, y/s);
+	}
+
+	double operator*(const Vec& v) const {
+		return x * v.x + y * v.y;
 	}
 
 	double x;
@@ -39,16 +48,62 @@ struct Circle {
 	}
 
 	bool inCircle(const Vec& v) {
-		return v.getDistance(center) <= (radii + FLT_EPSILON);
+		return abs(v.getDistance(center) - radii) <= epsilon;
 	};
 
 	Vec center;
-	double radii;
+	double radii = 0.0;
 };
 
-void setCircle(Circle& circle, const std::vector<unsigned int>& tri, const std::vector<Vec>& points) {
-	circle.center = (points[tri[0]] + points[tri[1]] + points[tri[2]]) / 3.0f;
-	circle.radii = circle.center.getDistance(points[tri[0]]);
+void setCircle(Circle& circle, const std::vector<unsigned int>& triangle, const std::vector<Vec>& points) {
+	double t[3];
+	for (int i = 0; i < 3; ++i) {
+		t[i] = points[triangle[i]] * points[triangle[i]];
+	}
+	Vec tri[3];
+	for (int i = 0; i < 3; ++i) {
+		tri[i] = points[triangle[i]];
+	}
+	double p =
+		tri[0].x * tri[1].y +
+		tri[1].x * tri[2].y +
+		tri[2].x * tri[0].y -
+		tri[0].x * tri[2].y -
+		tri[1].x * tri[0].y -
+		tri[2].x * tri[1].y;
+	Vec circum(
+		(t[1] * tri[2].y + t[0] * tri[1].y + t[2] * tri[0].y - t[1] * tri[0].y - t[2] * tri[1].y - t[0] * tri[2].y) / p / 2.0,
+		(t[2] * tri[1].x + t[1] * tri[0].x + t[0] * tri[2].x - t[0] * tri[1].x - t[1] * tri[2].x - t[2] * tri[0].x) / p / 2.0
+	);
+
+	Vec&& ad = circum - tri[0];
+	Vec&& ab = tri[1] - tri[0];
+	Vec&& ac = tri[2] - tri[0];
+
+	Vec max, min;
+	min.x = FLT_MAX; min.y = FLT_MAX;
+	max.x = FLT_MIN; max.y = FLT_MIN;
+	double max_distance = 0.0;
+	for (int i = 0; i < 3; ++i) {
+		auto d = tri[i].getDistance(tri[(i + 1) % 3]);
+		if ((d - max_distance) > -epsilon) {
+			max = tri[i];
+			min = tri[(i + 1) % 3];
+			max_distance = d;
+		}
+	}
+	circle.center = (min + max) / 2;
+	for (const auto& t : tri) {
+		auto r = circle.center.getDistance(t);
+		if(r > circle.radii) {
+			circle.radii = r;
+		}
+	}
+	auto cr = circum.getDistance(tri[0]);
+	if (cr < circle.radii) {
+		circle.center = circum;
+		circle.radii = cr;
+	}
 }
 
 int main() {
@@ -69,23 +124,22 @@ int main() {
 				triangle[1] = 1;
 				triangle[2] = 2;
 				setCircle(circle, triangle, points);
+				tri_init = true;
 			}
 			if (i > 2) {
 				if (!circle.inCircle(v)) {
-					Circle t_cs[3];
-					setCircle(t_cs[0], { triangle[0], triangle[1], i }, points); // 2
-					setCircle(t_cs[1], { triangle[1], triangle[2], i }, points); // 0
-					setCircle(t_cs[2], { triangle[2], triangle[0], i }, points); // 1
-					int max = 0;
-					int change = 2;
-					for (int ci = 1; ci < 3; ++ci) {
-						if (t_cs[ci].radii > t_cs[max].radii) {
-							max = ci;
-							change = (ci + 2) % 3;
+					Circle temp_circle;
+					for (unsigned int ti = 0; ti < i; ++ti) {
+						for (unsigned int tj = ti + 1; tj < i; ++tj) {
+							setCircle(temp_circle, { ti, tj, i }, points);
+							if ((temp_circle.radii - circle.radii) > 0.0) {
+								circle = temp_circle;
+								triangle[0] = ti;
+								triangle[1] = tj;
+								triangle[2] = i;
+							}
 						}
 					}
-					circle = t_cs[max];
-					triangle[change] = i;
 				}
 			}
 		}
@@ -97,7 +151,7 @@ int main() {
 		}
 		else if (n == 2) {
 			double distance = points[0].getDistance(points[1]);
-			if ((radii * 2.0 + FLT_EPSILON) >= (distance)) {
+			if ((radii * 2.0 - distance) <= epsilon) {
 				ans = true;
 			}
 			else {
@@ -105,7 +159,7 @@ int main() {
 			}
 		}
 		else {
-			if (circle.radii <= (radii + FLT_EPSILON)) {
+			if ((circle.radii - radii) <= epsilon) {
 				ans = true;
 			}
 			else {
