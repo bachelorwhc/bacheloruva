2106f1d3abb5af2719bdead6506de8928b4fed80
bachelor.whc@gmail.com <bachelor.whc@gmail.com>
2018-06-21 20:40:16 +0800

    294

--- 294.cpp
+++ 294.cpp
@@ -3,64 +3,107 @@
 #include<algorithm>
 #include<vector>
 #include<cmath>
+#ifdef _DEBUG
+#include<fstream>
+#endif
 
-const int max = 100000;
-int raw_table[ max ];
+const int MAX = 100000;
+int raw_table[MAX + 1];
 std::vector<int> table;
 
 void build_table()
 {
-    int i = sizeof( raw_table );
-    for ( int i = 0; i < max; ++i )
-        raw_table[ i ] = 1;
-    for ( int i = 2; i < max; ++i )
-    {
-        if ( raw_table[ i ] == 0 )
-            continue;
-        for ( int j = 2; i * j < max; ++j )
-            raw_table[ i * j ] = 0;
-    }
-    for ( int i = 2; i < max; ++i )
-    {
-        if ( raw_table[ i ] == 0 )
-            continue;
-        table.push_back( raw_table[ i ] );
-    }
+	int i = sizeof(raw_table);
+	for (int i = 0; i <= MAX; ++i)
+		raw_table[i] = 1;
+	for (int i = 2; i <= MAX; ++i)
+	{
+		if (raw_table[i] == 0)
+			continue;
+		for (int j = 2; i * j < MAX; ++j)
+			raw_table[i * j] = 0;
+	}
+	for (int i = 2; i <= MAX; ++i)
+	{
+		if (raw_table[i] == 0)
+			continue;
+		table.push_back(i);
+	}
 }
 
-void find_maximum_divisors( int min, int max, int& ans, int& divisor_count )
+void find_maximum_divisors(int min, int max, int& ans, int& divisor_count)
 {
-    divisor_count = 0;
-    ans = 0;
-    for ( int i = min; i <= max; ++i )
-    {
-        int c_divisor_count{ 0 };
+	divisor_count = 0;
+	ans = 0;
+	for (int number = min; number <= max; ++number)
+	{
+		if (number == 1)
+		{
+			divisor_count = 1;
+			ans = 1;
+			continue;
+		}
+		else if (number <= MAX && raw_table[number] > 0)
+		{
+			if (divisor_count < 2)
+			{
+				ans = number;
+				divisor_count = 2;
+			}
+			continue;
+		}
 
-        for ( auto p : table )
-        {
-            if(  )
-        }
+		int c_divisor_count{ 1 };
 
-        if ( i > ans && c_divisor_count >= divisor_count )
-        {
-            divisor_count = c_divisor_count;
-            ans = i;
-        }
-    }
+		std::vector<std::pair<int, int>> divisors;
+		for (auto p : table)
+		{
+			if (p > number)
+				break;
+			std::pair<int, int> divisor{p, 0};
+			auto n = number;
+			while (n % p == 0)
+			{
+				n /= p;
+				++divisor.second;
+			}
+			if(divisor.second > 0)
+				divisors.push_back(divisor);
+		}
+		for (auto& d : divisors) 
+		{
+			c_divisor_count *= (d.second + 1);
+		}
+
+		if (c_divisor_count > divisor_count)
+		{
+			divisor_count = c_divisor_count;
+			ans = number;
+		}
+	}
 }
 
 int main()
 {
-    build_table();
-    int n;
-    std::cin >> n;
-    for ( int i = 0; i < n; ++i )
-    {
-        int min, max;
-        std::cin >> min >> max;
-        int ans, divisor_count;
-        find_maximum_divisors( min, max, ans, divisor_count );
-        std::cout << ans << " " <<  divisor_count << std::endl;
-    }
-    return 0;
+	build_table();
+	int n;
+	std::cin >> n;
+#ifdef _DEBUG
+	std::ofstream file("ans.txt");
+#endif
+	for (int i = 0; i < n; ++i)
+	{
+		int min, max;
+		std::cin >> min >> max;
+		int ans, divisor_count;
+		find_maximum_divisors(min, max, ans, divisor_count);
+#ifdef _DEBUG
+		file
+#else
+		std::cout 
+#endif
+			<< "Between " << min << " and " << max << ", " 
+			<< ans << " has a maximum of " << divisor_count << " divisors." << std::endl;
+	}
+	return 0;
 }
