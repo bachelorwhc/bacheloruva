fd3ac8003fc191939304f8a991aebc075153d56d
bachelorwhc <bachelor.whc@gmail.com>
2018-07-10 16:01:07 +0800

    solved.

--- 369.cpp
+++ 369.cpp
@@ -1,28 +1,128 @@
 #include <iostream>
 #include <cmath>
 
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cstdint>
+#ifdef _DEBUG
+#include <fstream>
+#endif
+
+const uint64_t max = 33554432;
+bool table[ max ];
+
+void build_table()
+{
+    int i = sizeof( table );
+    for ( int i = 0; i < max; ++i )
+        table[ i ] = true;
+    for ( int i = 2; i < max; ++i )
+    {
+        if ( table[ i ] == 0 )
+            continue;
+        for ( int j = 2; i * j < max; ++j )
+            table[ i * j ] = false;
+    }
+}
+
+using g_priority_queue = std::priority_queue<uint64_t, std::vector<uint64_t>, std::greater<uint64_t>>;
 int main()
 {
-	unsigned long long m, n;
-	while (std::cin >> n >> m)
-	{
-		unsigned long long bm(m);
-		if(n == 0 && m == 0)
-			break;
-		if(n == m)
-		{
-			std::cout << n << " things taken " << m << " at a time is " << 1 << " exactly." << std::endl;
-			continue;
-		}
-		if(n - m < m)
-			m = n - m;
-		unsigned long long a(n - m + 1), b(1);
-		for(unsigned long long i = 2; (n - m) + i <= n; ++i)
-				a *= (n - m + i);
-		for(unsigned long long i = 2; i <= m; ++i)
-				b *= i;
-		unsigned long long ans(a/b);
-		std::cout << n << " things taken " << bm << " at a time is " << ans << " exactly." << std::endl;
-	}
-	return 0;
+    build_table();
+    uint64_t m, n;
+#ifdef _DEBUG
+    std::ofstream file( "ans.txt" );
+#endif
+    while ( std::cin >> m >> n && !( m == 0 && n == 0 ) )
+    {
+#ifdef _DEBUG
+        file << m << " things taken " << n << " at a time is ";
+#endif
+        std::cout << m << " things taken " << n << " at a time is ";
+        n = std::min( m - n, n );
+        g_priority_queue m_nonprime_queue;
+        g_priority_queue m_big_queue;
+        std::priority_queue<uint64_t> d_prime_queue;
+        std::priority_queue<uint64_t> d_nonprime_queue;
+        uint64_t pm{ 1 };
+        for ( uint64_t i = m - n + 1; i <= m; ++i )
+        {
+            if ( i >= max )
+                if ( i % 2 == 0 || i % 3 == 0 || i % 5 == 0 || i % 7 == 0 || i % 11 == 0 ||
+                     i % 13 == 0 || i % 17 == 0 || i % 19 == 0 || i % 23 == 0 )
+                    m_nonprime_queue.push( i );
+                else
+                    m_big_queue.push( i );
+            else if ( table[ i ] )
+                pm *= i;
+            else
+                m_nonprime_queue.push( i );
+        }
+        uint64_t npm{ 1 };
+        for ( uint64_t i = 2; i <= n; ++i )
+        {
+            if ( table[ i ] )
+                d_prime_queue.push( i );
+            else
+                d_nonprime_queue.push( i );
+        };
+        auto try_to_pop_all =[]( g_priority_queue& nonprime_queue, std::priority_queue<uint64_t>& d_queue )
+        {
+            while ( nonprime_queue.size() > 1 && d_queue.size() > 0 )
+            {
+                auto top = nonprime_queue.top();
+                nonprime_queue.pop();
+                auto dp = d_queue.top();
+                if ( top % dp == 0 )
+                {
+                    top /= dp;
+                    d_queue.pop();
+                }
+                else
+                {
+                    top *= nonprime_queue.top();
+                    nonprime_queue.pop();
+                }
+                nonprime_queue.push( top );
+            }
+        };
+
+        try_to_pop_all( m_nonprime_queue, d_nonprime_queue );
+        try_to_pop_all( m_nonprime_queue, d_prime_queue );
+        try_to_pop_all( m_big_queue, d_nonprime_queue );
+        try_to_pop_all( m_big_queue, d_prime_queue );
+
+        uint64_t ans{ pm };
+        while ( m_nonprime_queue.size() )
+        {
+            ans *= m_nonprime_queue.top();
+            m_nonprime_queue.pop();
+        }
+        while ( m_big_queue.size() )
+        {
+            ans *= m_big_queue.top();
+            m_big_queue.pop();
+        }
+        while ( d_nonprime_queue.size() )
+        {
+            ans /= d_nonprime_queue.top();
+            d_nonprime_queue.pop();
+        }
+        while ( d_prime_queue.size() )
+        {
+            ans /= d_prime_queue.top();
+            d_prime_queue.pop();
+        }
+        std::cout << ans << " exactly." << std::endl;
+#ifdef _DEBUG
+        file << ans << " exactly." << std::endl;
+#endif
+    }
+#ifdef _DEBUG
+    file.close();
+#endif
+    return 0;
 }
--- 530.cpp
+++ 530.cpp
@@ -8,49 +8,114 @@
 #include <fstream>
 #endif
 
-const int max = 10000;
-int table[ max ];
+const uint64_t max = 33554432;
+bool table[ max ];
 
 void build_table()
 {
     int i = sizeof( table );
     for ( int i = 0; i < max; ++i )
-        table[ i ] = 1;
+        table[ i ] = true;
     for ( int i = 2; i < max; ++i )
     {
         if ( table[ i ] == 0 )
             continue;
         for ( int j = 2; i * j < max; ++j )
-            table[ i * j ] = 0;
+            table[ i * j ] = false;
     }
 }
 
+using g_priority_queue = std::priority_queue<uint64_t, std::vector<uint64_t>, std::greater<uint64_t>>;
 int main()
 {
     build_table();
-    int m, n;
+    uint64_t m, n;
+#ifdef _DEBUG
+    std::ofstream file( "ans.txt" );
+#endif
     while ( std::cin >> m >> n && !( m == 0 && n == 0 ) )
     {
-        std::priority_queue<uint64_t, std::vector<uint64_t>, std::greater<uint64_t>> dqueue;
-        std::priority_queue<uint64_t> aqueue;
+        n = std::min( m - n, n );
+        g_priority_queue m_nonprime_queue;
+        g_priority_queue m_big_queue;
+        std::priority_queue<uint64_t> d_prime_queue;
+        std::priority_queue<uint64_t> d_nonprime_queue;
         uint64_t pm{ 1 };
-        for ( uint64_t i = n + 1; i <= m; ++i )
+        for ( uint64_t i = m - n + 1; i <= m; ++i )
         {
-            if ( table[ i ] )
+            if ( i >= max )
+                if ( i % 2 == 0 || i % 3 == 0 || i % 5 == 0 || i % 7 == 0 || i % 11 == 0 ||
+                     i % 13 == 0 || i % 17 == 0 || i % 19 == 0 || i % 23 == 0 )
+                    m_nonprime_queue.push( i );
+                else
+                    m_big_queue.push( i );
+            else if ( table[ i ] )
                 pm *= i;
             else
-                dqueue.push( i );
+                m_nonprime_queue.push( i );
         }
         uint64_t npm{ 1 };
         for ( uint64_t i = 2; i <= n; ++i )
         {
             if ( table[ i ] )
-                npm *= i;
+                d_prime_queue.push( i );
             else
-                aqueue.push( i );
+                d_nonprime_queue.push( i );
         };
-        //ans /= _n;
-        std::cout << pm << ' ' << npm << std::endl;
+        auto try_to_pop_all =[]( g_priority_queue& nonprime_queue, std::priority_queue<uint64_t>& d_queue )
+        {
+            while ( nonprime_queue.size() > 1 && d_queue.size() > 0 )
+            {
+                auto top = nonprime_queue.top();
+                nonprime_queue.pop();
+                auto dp = d_queue.top();
+                if ( top % dp == 0 )
+                {
+                    top /= dp;
+                    d_queue.pop();
+                }
+                else
+                {
+                    top *= nonprime_queue.top();
+                    nonprime_queue.pop();
+                }
+                nonprime_queue.push( top );
+            }
+        };
+
+        try_to_pop_all( m_nonprime_queue, d_nonprime_queue );
+        try_to_pop_all( m_nonprime_queue, d_prime_queue );
+        try_to_pop_all( m_big_queue, d_nonprime_queue );
+        try_to_pop_all( m_big_queue, d_prime_queue );
+
+        uint64_t ans{ pm };
+        while ( m_nonprime_queue.size() )
+        {
+            ans *= m_nonprime_queue.top();
+            m_nonprime_queue.pop();
+        }
+        while ( m_big_queue.size() )
+        {
+            ans *= m_big_queue.top();
+            m_big_queue.pop();
+        }
+        while ( d_nonprime_queue.size() )
+        {
+            ans /= d_nonprime_queue.top();
+            d_nonprime_queue.pop();
+        }
+        while ( d_prime_queue.size() )
+        {
+            ans /= d_prime_queue.top();
+            d_prime_queue.pop();
+        }
+        std::cout << ans << std::endl;
+#ifdef _DEBUG
+        file << ans << std::endl;
+#endif
     }
+#ifdef _DEBUG
+    file.close();
+#endif
     return 0;
 }
